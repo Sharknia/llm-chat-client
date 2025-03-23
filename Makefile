f9615bce11ccf00d0ef7096c77c77b832f1c25a3
sharknia <furychick0@gmail.com>
2025-03-23 19:15:31 +0900

    Makefile 수정 및 FastAPI 애플리케이션 추가
    - Makefile에서 애플리케이션 경로를 수정하여 실행 명령어를 업데이트하였음.
    - app/main.py 파일을 새로 추가하여 FastAPI 애플리케이션을 구현하고, 기본 루트 경로에서 "Hello, FastAPI!" 메시지를 반환하는 기능을 추가하였음.

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 run:
-	poetry run uvicorn main:app --reload
+	poetry run uvicorn app.main:app --reload
---
+++ app/main.py
@@ -0,0 +1,9 @@
+# app/main.py
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello, FastAPI!"}
