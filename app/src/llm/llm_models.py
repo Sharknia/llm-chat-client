093fb2d0f8c3701d8b6c7273faf9ac061ff0c23d
sharknia <furychick0@gmail.com>
2025-04-21 21:46:21 +0900

    LLMModelListEnum에 GEMINI_1_5_PRO 추가 및 GeminiModels 클래스 구현
    - LLMModelListEnum에 새로운 모델인 GEMINI_1_5_PRO를 추가하였으며, GeminiModels 클래스를 구현하여 API 키와 기본 URL을 설정하고 모델을 가져오는 메서드를 추가함.

--- app/src/llm/llm_models.py
+++ app/src/llm/llm_models.py
@@ -9,11 +9,9 @@ load_dotenv()
 
 
 class LLMModelListEnum(enum.Enum):
-    GROK2_VISION_1212 = "grok-2-vision-1212"
-    GROK2_1212 = "grok-2-1212"
     GROK3_MINI_BETA = "grok-3-mini-beta"
     GROK3_LATEST = "grok-3-latest"
-
+    GEMINI_1_5_PRO = "gemini-1.5-pro"
 
 class LlmModels(ABC):
     @abstractmethod
@@ -43,6 +41,24 @@ class GrokModels(LlmModels):
     def get_model(
         self,
         model_enum: LLMModelListEnum = model_enum.GROK3_MINI_BETA.value,
+    ):
+        return model_enum
+
+    def get_base_url(self):
+        return self.base_url
+
+
+class GeminiModels(LlmModels):
+    api_key = os.getenv("GEMINI_API_KEY", "null")
+    model_enum = LLMModelListEnum
+    base_url = "https://generativelanguage.googleapis.com/v1beta"
+
+    def get_api_key(self):
+        return self.api_key
+
+    def get_model(
+        self,
+        model_enum: LLMModelListEnum = model_enum.GEMINI_1_5_PRO.value,
     ):
         return model_enum
 
