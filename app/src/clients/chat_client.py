6eea53966f0e44721d357f12e57a705f6c5dafff
sharknia <furychick0@gmail.com>
2025-04-21 21:19:29 +0900

    ChatClient 클래스에 ChatCompletion 타입 추가 및 기본 모델 이름을 GROK3_LATEST로 설정
    - ChatClient 클래스에서 llm_model_name의 기본 값을 GROK3_LATEST로 변경하여 초기화 과정을 개선함.
    - get_completion 메서드에서 ChatCompletion 타입을 명시하고, 스트리밍 응답을 처리하는 로직을 추가하여 응답의 실시간 출력을 가능하게 함.

--- app/src/clients/chat_client.py
+++ app/src/clients/chat_client.py
@@ -1,4 +1,5 @@
 from openai import OpenAI
+from openai.types.chat.chat_completion import ChatCompletion
 
 from app.src.llm.llm_models import LLMModelListEnum, LlmModels
 
@@ -8,7 +9,7 @@ class ChatClient:
         self,
         llm_model: LlmModels,
         messages: list,
-        llm_model_name: LLMModelListEnum = None,
+        llm_model_name: LLMModelListEnum = LLMModelListEnum.GROK3_LATEST.value,
     ):
         self.llm_model = llm_model
         self.llm_model_name = llm_model.get_model(llm_model_name)
@@ -20,11 +21,18 @@ class ChatClient:
 
     def get_completion(self):
         try:
-            completion = self.client.chat.completions.create(
+            completion: ChatCompletion = self.client.chat.completions.create(
                 model=self.llm_model_name,
                 messages=self.messages,
+                temperature=1.0,
+                max_tokens=2500,
+                top_p=0.95,
+                stream=True,
             )
-            return completion.choices[0].message.content
+            for chunk in completion:
+                if chunk.choices[0].finish_reason == "stop":
+                    break
+                if chunk.choices[0].delta.content is not None:
+                    print(chunk.choices[0].delta.content, end="", flush=True)
         except Exception as e:
-            print("예외가 발생했습니다:", e)
             return f"예외가 발생했습니다: {e}"
