b12a78021c666c56285c6131bad3808ee392b60b
sharknia <furychick0@gmail.com>
2025-04-25 21:46:25 +0900

    Vercel 배포 설정 파일 추가 및 FastAPI에서 정적 파일 서빙 관련 코드 제거
    - vercel.json 파일 추가: 정적 파일 서빙을 위한 Vercel 설정 정의
    - FastAPI 애플리케이션에서 정적 파일 서빙 관련 코드 제거하여 코드 간소화

--- app/main.py
+++ app/main.py
@@ -1,23 +1,15 @@
 # app/main.py
-import os
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, Request
 from fastapi.openapi.utils import get_openapi
-from fastapi.responses import FileResponse, JSONResponse
-from fastapi.staticfiles import StaticFiles
+from fastapi.responses import JSONResponse
 
 from app.src.core.exceptions.base_exceptions import BaseHTTPException
 from app.src.core.logger import logger
 from app.src.domain.user.v1 import router as user_router
 
 app = FastAPI()
-
-# static 디렉토리 경로 설정 (app 디렉토리 기준)
-static_dir = os.path.join(os.path.dirname(__file__), "../static")
-
-# /static 경로를 static 디렉토리에 마운트
-app.mount("/static", StaticFiles(directory=static_dir), name="static")
 
 
 # Lifespan 핸들러
@@ -30,45 +22,6 @@ async def lifespan(app: FastAPI):
 
     # 애플리케이션 종료
     logger.info("애플리케이션 종료...")
-
-
-@app.get("/")
-async def read_index():
-    file_path = os.path.join(static_dir, "index.html")
-    if os.path.exists(file_path):
-        return FileResponse(file_path)
-    else:
-        from fastapi.responses import HTMLResponse
-
-        return HTMLResponse(
-            content="<h1>Error: index.html not found</h1>", status_code=404
-        )
-
-
-@app.get("/login")
-async def read_login():
-    file_path = os.path.join(static_dir, "login.html")
-    if os.path.exists(file_path):
-        return FileResponse(file_path)
-    else:
-        from fastapi.responses import HTMLResponse
-
-        return HTMLResponse(
-            content="<h1>Error: login.html not found</h1>", status_code=404
-        )
-
-
-@app.get("/signup")
-async def read_signup():
-    file_path = os.path.join(static_dir, "signup.html")
-    if os.path.exists(file_path):
-        return FileResponse(file_path)
-    else:
-        from fastapi.responses import HTMLResponse
-
-        return HTMLResponse(
-            content="<h1>Error: signup.html not found</h1>", status_code=404
-        )
 
 
 # Custom OpenAPI 설정
